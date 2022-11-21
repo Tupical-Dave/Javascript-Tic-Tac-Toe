4f2a10ead937b400b3a55e913395ac1740a90795
Tupical <d.avakian95@gmail.com>
2022-11-21 23:34:01 +0400

    build: rebuilt and refactored the whole thing.

--- index.js
+++ index.js
@@ -1,19 +1,88 @@
-let player = "cross";
+let playerTurn = true;
+const CIRCLE_CLASS = "circle";
+const CROSS_CLASS = "cross";
+const WINING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  [1, 4, 7],
+  [0, 3, 6],
+  [2, 5, 8],
+];
 
-function putTheSymbol(event) {
-  const element = event.target;
-  if (
-    element.classList.contains("cross") ||
-    element.classList.contains("circle")
-  ) {
-  } else {
-    const symbol = document.createElement("div");
-    symbol.classList.add(player);
-    element.appendChild(symbol);
-    if (player == "cross") {
-      player = "circle";
+const boardCells = document.querySelectorAll("[data-cell]");
+const board = document.getElementById("board");
+const endScreen = document.getElementById("end-screen");
+const endText = document.getElementById("end-text");
+const restartbutton = document.getElementById("restart-button");
+
+start();
+
+restartbutton.addEventListener("click", start);
+
+function clickHandling(event) {
+  // putting the symbol
+  const target = event.target;
+  let currentClass = playerTurn ? "cross" : "circle";
+  putTheSymbol(target, currentClass);
+  // check winner
+
+  if (checkWinner(currentClass)) {
+    endScreen.classList.add("show");
+    if (playerTurn) {
+      endText.innerHTML = `The Winner is X player!`;
     } else {
-      player = "cross";
+      endText.innerHTML = `The Winner is O player!`;
     }
   }
+  // changing turn
+  nextTurn();
+
+  // seting the hover class for the whole board
+  setBoardHoverClass();
+}
+
+function putTheSymbol(target, symbol) {
+  target.classList.add(symbol);
+}
+
+function nextTurn() {
+  playerTurn = !playerTurn;
+}
+
+function setBoardHoverClass() {
+  if (board.classList.contains("cross")) {
+    board.classList.replace("cross", "circle");
+  } else if (board.classList.contains("circle")) {
+    board.classList.replace("circle", "cross");
+  }
+}
+
+function checkWinner(currentClass) {
+  return WINING_COMBINATIONS.some((combination) => {
+    return combination.every((index) => {
+      return boardCells[index].classList.contains(currentClass);
+    });
+  });
+}
+
+function start() {
+  boardCells.forEach((element) => {
+    element.classList.remove("cross");
+    element.classList.remove("circle");
+  });
+
+  endScreen.classList.remove("show");
+
+  if (board.classList.contains("circle")) {
+    board.classList.replace("circle", "cross");
+  }
+
+  boardCells.forEach((element) => {
+    element.addEventListener("click", clickHandling, { once: true });
+  });
+
+  playerTurn = true;
 }
